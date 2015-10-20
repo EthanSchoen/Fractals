e6782a04c158c88fa6c46bdfd7b63cec50b48402
Ethan Schoen <e.schoen@yahoo.com>
2015-10-19 20:14:28 -0400

    Sierpinsky Triangle now working

--- FractApp.py
+++ FractApp.py
@@ -1,18 +1,56 @@
 import pygame
 import sys
 
+def drawTri(screen, pointList):
+    pygame.draw.lines(screen, (255,255,255), True, pointList, 1)
+
+def drawTriFract(screen, tri, layers):
+    layers -= 1
+    pygame.display.update()
+
+    pA = tri[0]
+    pB = tri[1]
+    pC = tri[2]
+
+    mAB = [(pA[0] + pB[0])/2, (pA[1] + pB[1])/2]
+    mBC = [(pC[0] + pB[0])/2, (pC[1] + pB[1])/2]
+    mAC = [(pA[0] + pC[0])/2, (pA[1] + pC[1])/2]
+
+    nTri = (mAB, mBC, mAC)
+    drawTri(screen, nTri)
+
+    if layers > 0:
+        drawTriFract(screen, (pA, mAB, mAC), layers)
+        drawTriFract(screen, (pB, mAB, mBC), layers)
+        drawTriFract(screen, (pC, mBC, mAC), layers)
+
 def main():
     windowWidth = 1200
     windowHeight = 600
     screen = pygame.display.set_mode((windowWidth, windowHeight))
     screen.fill((0,0,0))
-    pygame.display.update()
+
+    #Initialize Fractal
+    pygame.draw.lines(screen, (255,255,255), True, ([350,454], [600,100], [850,454]), 3)
+    triSide = 500
+    triangle = ([350,454], [600,100], [850,454])
+    i = 1    
+    maxV = False
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        
+        if i < 6:
+            i += 1
+        else:
+            maxV = True
+
+        if not maxV:
+            drawTriFract(screen, triangle, i)
+
         pygame.display.update()
 
 if __name__ == "__main__":
